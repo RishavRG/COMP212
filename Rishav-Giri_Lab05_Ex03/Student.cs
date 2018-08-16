e83382bd16780f08890f22d16e4c86cb6e378c53
R_Giri <10618753+RishavRG@users.noreply.github.com>
2018-08-16 18:25:09 -0400

    First commit

---
+++ Rishav-Giri_Lab05_Ex03/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rishav_Giri_Lab05_Ex03
+{
+    class Student
+    {
+    //public properties
+    public int ID { get; set; }
+    public string Name { get; set; }
+
+    //default constructor
+    public Student() : this(0, "no-name") { }
+
+    //constructor which takes arguments
+    public Student(int id, string name)
+    {
+        this.ID = id;
+        this.Name = name;
+    }
+
+    //overriding ToString
+    public override string ToString()
+    {
+        return $"ID: {this.ID}\tName: {this.Name}";
+    }
+}
+}
---
+++ Rishav-Giri_Lab05_Ex04/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rishav_Giri_Lab05_Ex04
+{
+    class Employee
+    {
+        //class variable
+        public static int numberOfEmployees = 0;
+
+    //public properties
+    public int Id { get; }
+    public string Name { get; set; }
+    private double salary;
+    public double Salary
+    {
+        get
+        {
+            return salary;
+        }
+        set
+        {
+            if (value > 0)
+                salary = value;
+            else
+                salary = 0.0;
+        }
+    }
+
+    // constructor
+    public Employee(string name, double sal)
+    {
+        this.Id = numberOfEmployees++;
+        Name = name;
+        Salary = sal;
+    }
+
+    //ToString()
+    public override string ToString()
+    {
+        return $"ID #{Id}, Name: {Name}, Salary: {Salary:C}";
+    }
+}
+}
